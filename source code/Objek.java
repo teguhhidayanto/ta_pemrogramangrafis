8204a8ae9828056aaf59e05de3f0841e1bf4cc89
ardyanreza <39753586+ardyanreza@users.noreply.github.com>
2018-07-10 15:11:06 +0700

    Tambah badan mobil
    Menambahkan objek berupa persegi

--- source code/Objek.java
+++ source code/Objek.java
@@ -62,5 +62,51 @@ static void Sayap(GL gl)
   gl.glVertex3f(-1, 0, -0.5f);
   gl.glEnd();
 }
-  
+  static void Persegi(GL gl) {
+        float amb[] = {0, 0.5f, 0, 1};
+        float diff[] = {0, 0.5f, 0, 1};
+        float spec[] = {0, 0.5f, 0, 1};
+        float shine = 0;
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT, amb, 0);
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_DIFFUSE, diff, 0);
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, spec, 0);
+        gl.glMaterialf(GL.GL_FRONT_AND_BACK, GL.GL_SHININESS, shine);
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(0, 0, 0);
+        gl.glVertex3f(0, 0, 2);
+        gl.glVertex3f(2, 0, 2);
+        gl.glVertex3f(2, 0, 0);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(0, 0, 0);
+        gl.glVertex3f(0, 4, 0);
+        gl.glVertex3f(2, 4, 0);
+        gl.glVertex3f(2, 0, 0);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(0, 4, 0);
+        gl.glVertex3f(2, 4, 0);
+        gl.glVertex3f(2, 4, 2);
+        gl.glVertex3f(0, 4, 2);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(0, 4, 2);
+        gl.glVertex3f(2, 4, 2);
+        gl.glVertex3f(2, 0, 2);
+        gl.glVertex3f(0, 0, 2);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(2, 4, 2);
+        gl.glVertex3f(2, 4, 0);
+        gl.glVertex3f(2, 0, 0);
+        gl.glVertex3f(2, 0, 2);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(0, 4, 2);
+        gl.glVertex3f(0, 4, 0);
+        gl.glVertex3f(0, 0, 0);
+        gl.glVertex3f(0, 0, 2);
+        gl.glEnd();
+        
+    }
 }
